31a31cba0946f930972c6d2371e72c5fa3a03e7a
jyaba <bishalnepali4@gmail.com>
2023-05-13 22:42:55 +0545

    Added the muliple

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,12 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./js/*.js');
     eleventyConfig.addPassthroughCopy("admin/");
 
+    eleventyConfig.addPassthroughCopy("css/");
+    eleventyConfig.addPassthroughCopy("images/");
+    eleventyConfig.addPassthroughCopy("fonts/");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+
+
     // ... other Eleventy configurations ...
 
     return {
